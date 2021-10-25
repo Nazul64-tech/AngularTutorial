f7938cc3747d88dbe757256b27c2642242add9b0
Jcarpenter <Jcarpenter@catalyte.io>
2021-10-25 13:13:30 -0700

    added localstorage service

--- todo-list/src/app/services/todo-list.service.ts
+++ todo-list/src/app/services/todo-list.service.ts
@@ -1,22 +1,50 @@
 import { Injectable } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import { StorageService } from './storage.service';
+
+const todoListStorageKey = 'Todo_List';
+const defaultTodoList: TodoItem[] = [
+  {title: 'install NodeJS'},
+  {title: 'install Angular CLI'},
+  {title: 'create new app'},
+  {title: 'serve app'},
+  {title: 'develop app'},
+  {title: 'deploy app'},
+];
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
-  private todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
+  todoList: TodoItem[];
 
-  constructor() { }
+  constructor(private storageService: StorageService) {
+    this.todoList = 
+    storageService.getData(todoListStorageKey) || defaultTodoList;
+  }
+
+  saveList(): void {
+    this.storageService.setData(todoListStorageKey, this.todoList);
+  }
 
   getTodoList(): TodoItem[] {
     return this.todoList;
+  }
+
+  addItem(newItem: TodoItem): void {
+    this.todoList.push(newItem);
+    this.saveList();
+  }
+
+  updateItem(item: TodoItem, changes: any): void {
+    const index = this.todoList.indexOf(item);
+    this.todoList[index] = {...item, ...changes};
+    this.saveList();
+  }
+
+  deleteItem(item: TodoItem): void {
+    const index = this.todoList.indexOf(item);
+    this.todoList.splice(index, 1);
+    this.saveList();
   }
 }
